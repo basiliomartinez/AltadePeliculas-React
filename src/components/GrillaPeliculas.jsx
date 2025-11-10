8512cdf376357aeee4f36fb434c2784263c30fa9
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-11-10 07:52:17 -0300

    Trabajamos con el local storage

--- src/components/GrillaPeliculas.jsx
+++ src/components/GrillaPeliculas.jsx
@@ -1,10 +1,13 @@
 import{Row} from"react-bootstrap"
 import CardPelicula from "./CardPelicula";
 
-const GrillaPeliculas = () => {
+const GrillaPeliculas = ({peliculas}) => {
     return (
               <Row xs={1} md={2} lg={4} className="mt-4 gap-3">
-       <CardPelicula></CardPelicula>
+                {
+                    peliculas.map((peliculas)=><CardPelicula key={peliculas.id} pelicula={peliculas}></CardPelicula>)
+                }
+       
     
       </Row>
     );
